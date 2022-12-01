6ef23d80c083a43ae9854d71f178534033af14df
kugiyasan <kugiyasan@users.noreply.github.com>
2022-12-01 00:24:25 -0500

    day 1 part 2 done

--- d1_calorie_counting/src/main.rs
+++ d1_calorie_counting/src/main.rs
@@ -5,18 +5,20 @@ fn main() {
     let mut buf = String::new();
     File::open(path).unwrap().read_to_string(&mut buf).unwrap();
 
-    // let input: Vec<Vec<i32>> = buf
-    //     .trim()
-    //     .split("\n\n")
-    //     .map(|s| s.split('\n').map(|line| line.parse().unwrap()).collect())
-    //     .collect();
-
-    let input: i32 = buf
+    let mut input: Vec<i32> = buf
         .trim()
         .split("\n\n")
         .map(|s| s.split('\n').map(|line| line.parse::<i32>().unwrap()).sum())
-        .max()
-        .unwrap();
+        .collect();
 
-    println!("{:?}", input);
+    println!("{}", input.iter().max().unwrap());
+
+    // part 2
+
+    input.sort();
+    input.reverse();
+
+    let sum: i32 = input.iter().take(3).sum();
+
+    println!("{sum}");
 }
