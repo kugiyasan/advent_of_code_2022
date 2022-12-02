0195a67e85caa443a30b4b5792ff046be0825b71
kugiyasan <kugiyasan@users.noreply.github.com>
2022-12-02 01:28:20 -0500

    day 2 part 2

--- d2_rock_paper_scissors/src/main.rs
+++ d2_rock_paper_scissors/src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, io::Read, str::FromStr};
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 enum Hand {
     Rock,
     Paper,
@@ -35,13 +35,7 @@ impl Hand {
     }
 
     fn get_score(&self, other: &Hand) -> i32 {
-        let score = match self.fight(other) {
-            Outcome::Win => 6,
-            Outcome::Loss => 0,
-            Outcome::Draw => 3,
-        };
-
-        score + self.get_point()
+        self.fight(other).get_point() + self.get_point()
     }
 }
 
@@ -57,33 +51,89 @@ impl FromStr for Hand {
     }
 }
 
-fn main() {
-    let path = "input";
-    let mut buf = String::new();
-    File::open(path).unwrap().read_to_string(&mut buf).unwrap();
+impl Outcome {
+    fn get_point(&self) -> i32 {
+        match self {
+            Outcome::Win => 6,
+            Outcome::Loss => 0,
+            Outcome::Draw => 3,
+        }
+    }
 
-    let input: Vec<Vec<Hand>> = buf
+    fn find_other_hand(&self, other: &Hand) -> Hand {
+        match (self, other) {
+            (Outcome::Win, Hand::Rock) => Hand::Paper,
+            (Outcome::Win, Hand::Paper) => Hand::Scissors,
+            (Outcome::Win, Hand::Scissors) => Hand::Rock,
+            (Outcome::Loss, Hand::Rock) => Hand::Scissors,
+            (Outcome::Loss, Hand::Paper) => Hand::Rock,
+            (Outcome::Loss, Hand::Scissors) => Hand::Paper,
+            (Outcome::Draw, _) => other.clone(),
+        }
+    }
+
+    fn get_score(&self, other: &Hand) -> i32 {
+        self.find_other_hand(other).get_point() + self.get_point()
+    }
+}
+
+impl FromStr for Outcome {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Outcome, ()> {
+        match s {
+            "X" => Ok(Outcome::Loss),
+            "Y" => Ok(Outcome::Draw),
+            "Z" => Ok(Outcome::Win),
+            _ => Err(()),
+        }
+    }
+}
+
+fn part1(buf: &str) {
+    let input: Vec<(Hand, Hand)> = buf
         .trim()
         .split('\n')
         .map(|s| {
-            s.split(' ')
-                .map(|hand| hand.parse::<Hand>().unwrap())
-                .collect()
+            let line: Vec<_> = s.split(' ').collect();
+            let hand = line[0].parse::<Hand>().unwrap();
+            let other = line[1].parse::<Hand>().unwrap();
+            (hand, other)
         })
         .collect();
 
     let total_score: i32 = input
         .iter()
-        .map(|hands| hands[1].get_score(&hands[0]))
+        .map(|(other, hand)| hand.get_score(&other))
         .sum();
 
-    // let total_score: Vec<i32> = input
-    //     .iter()
-    //     .map(|hands| hands[1].get_score(&hands[0]))
-    //     .collect();
-
-    // println!("{:?}", &input[0..5]);
-    // println!("{:?}", &total_score[0..5]);
     println!("{}", total_score);
-    // println!("{:?}", input);
+}
+
+fn part2(buf: &str) {
+    let input: Vec<(Hand, Outcome)> = buf
+        .trim()
+        .split('\n')
+        .map(|s| {
+            let line: Vec<_> = s.split(' ').collect();
+            let hand = line[0].parse::<Hand>().unwrap();
+            let outcome = line[1].parse::<Outcome>().unwrap();
+            (hand, outcome)
+        })
+        .collect();
+
+    let total_score: i32 = input
+        .iter()
+        .map(|(hand, outcome)| outcome.get_score(&hand))
+        .sum();
+
+    println!("{}", total_score);
+}
+
+fn main() {
+    let path = "input";
+    let mut buf = String::new();
+    File::open(path).unwrap().read_to_string(&mut buf).unwrap();
+
+    // part1(&buf);
+    part2(&buf);
 }
