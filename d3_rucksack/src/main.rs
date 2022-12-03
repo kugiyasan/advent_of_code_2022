16cb6ddf92bb2056612b8b2e4a086cfc67bf1dbf
kugiyasan <kugiyasan@users.noreply.github.com>
2022-12-03 00:48:30 -0500

    day 3 part 2

--- d3_rucksack/src/main.rs
+++ d3_rucksack/src/main.rs
@@ -1,9 +1,14 @@
 use std::{collections::HashSet, fs};
 
-fn main() {
-    let path = "input";
-    // let path = "example";
-    let buf = fs::read_to_string(path).unwrap();
+fn get_letter_priority(letter: char) -> i32 {
+    if letter.is_lowercase() {
+        (letter as u8 - 'a' as u8 + 1) as i32
+    } else {
+        (letter as u8 - 'A' as u8 + 27) as i32
+    }
+}
+
+fn part1(buf: &str) {
     let input: Vec<(&str, &str)> = buf
         .trim()
         .split('\n')
@@ -15,18 +20,53 @@ fn main() {
         .map(|(compartment1, compartment2)| {
             let set = HashSet::<_>::from_iter(compartment1.chars());
             let letter = compartment2.chars().find(|c| set.contains(c)).unwrap();
-            if letter.is_lowercase() {
-                (letter as u8 - 'a' as u8 + 1) as i32
-            } else {
-                (letter as u8 - 'A' as u8 + 27) as i32
-            }
+            get_letter_priority(letter)
         })
         .collect();
 
     let sum: i32 = result.iter().sum();
-
-    // println!("{:?}", input);
-    // println!("{:?}", input[0]);
-    // println!("{:?}", &result[0..5]);
     println!("{}", sum);
+}
+
+fn part2(buf: &str) {
+    let input: Vec<&str> = buf.trim().split('\n').collect();
+    let result: Vec<_> = input
+        .chunks(3)
+        .map(|elves| {
+            if let &[elf1, elf2, elf3] = elves {
+                let set1 = HashSet::<_>::from_iter(elf1.chars());
+                let set2 = HashSet::<_>::from_iter(elf2.chars());
+                let set3 = HashSet::<_>::from_iter(elf3.chars());
+
+                let intersection1: HashSet<_> = set1
+                    .intersection(&set2)
+                    .map(|c| c.to_owned())
+                    .collect::<HashSet<_>>();
+                let letters: Vec<_> = intersection1.intersection(&set3).collect();
+
+                if let Some(first) = letters.first() {
+                    **first
+                } else {
+                    panic!("There's more than one letter");
+                }
+            } else {
+                panic!("Not 3 lines");
+            }
+        })
+        .collect();
+
+    let sum: i32 = result
+        .iter()
+        .map(|&letter| get_letter_priority(letter))
+        .sum();
+
+    println!("{}", sum);
+}
+
+fn main() {
+    let path = "input";
+    // let path = "example";
+    let buf = fs::read_to_string(path).unwrap();
+    part1(&buf);
+    part2(&buf);
 }
