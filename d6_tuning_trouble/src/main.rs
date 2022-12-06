babc7712205ecf324f38c84a44e2553116724b68
kugiyasan <kugiyasan@users.noreply.github.com>
2022-12-06 00:16:21 -0500

    day 6

---
+++ d6_tuning_trouble/src/main.rs
@@ -0,0 +1,22 @@
+use std::{collections::HashSet, fs};
+
+fn main() {
+    let path = "input";
+    let buf = fs::read_to_string(path).unwrap();
+    // let buf = "bvwbjplbgvbhsrlpgdmjqwftvncz".to_string();
+
+    // let unique_chars = 4;
+    let unique_chars = 14;
+
+    let index = buf
+        .chars()
+        .collect::<Vec<_>>()
+        .windows(unique_chars)
+        .position(|arr| {
+            let hashSet = HashSet::<_>::from_iter(arr.iter());
+            hashSet.len() == unique_chars
+        })
+        .unwrap();
+
+    println!("{:?}", index + unique_chars);
+}
