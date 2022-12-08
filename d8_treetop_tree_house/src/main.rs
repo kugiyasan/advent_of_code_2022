10bc46b4da99688f6e73c5b9a92c80d35b713c15
kugiyasan <kugiyasan@users.noreply.github.com>
2022-12-08 01:17:26 -0500

    day 8 part 2 first attempt

--- d8_treetop_tree_house/src/main.rs
+++ d8_treetop_tree_house/src/main.rs
@@ -1,8 +1,8 @@
 use std::fs;
 
 fn main() {
-    let path = "input";
-    // let path = "example";
+    // let path = "input";
+    let path = "example";
     let buf = fs::read_to_string(path).unwrap();
 
     let input: Vec<Vec<_>> = buf
@@ -11,12 +11,17 @@ fn main() {
         .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
         .collect();
 
+    // println!("input:\n{}", buf);
+    part1(&input);
+    part2(&input);
+}
+
+fn part1(input: &Vec<Vec<u32>>) {
     // height = 99, width = 99
     let height = input.len();
     let width = input[0].len();
     // println!("height: {}, width: {}", height, width);
     let mut seen = vec![vec![false; width]; height];
-
     // top and bottom
     for x in 1..width - 1 {
         let mut max_tree_height = input[0][x];
@@ -35,7 +40,6 @@ fn main() {
             }
         }
     }
-
     // left and right
     for y in 1..height - 1 {
         let mut max_tree_height = input[y][0];
@@ -55,7 +59,6 @@ fn main() {
             }
         }
     }
-
     println!(
         "seen:\n{}",
         seen.iter()
@@ -72,6 +75,71 @@ fn main() {
         .sum();
     println!("{}", res);
     let res = res + height * 2 + (width - 2) * 2;
-    println!("input:\n{}", buf);
     println!("{}", res);
+}
+
+fn part2(input: &Vec<Vec<u32>>) {
+    let height = input.len();
+    let width = input[0].len();
+    let max = (0..height)
+        .into_iter()
+        .map(|y| {
+            (0..width)
+                .into_iter()
+                .map(|x| scenic_score(input, y, x))
+                .max()
+                .unwrap()
+        })
+        .max()
+        .unwrap();
+
+    println!("max: {}", max);
+}
+
+fn scenic_score(input: &Vec<Vec<u32>>, tree_y: usize, tree_x: usize) -> u32 {
+    let height = input.len();
+    let width = input[0].len();
+
+    let mut count_above = 0;
+    let mut max_tree_height = 0;
+    for y in (0..tree_y).rev() {
+        if input[y][tree_x] > max_tree_height {
+            count_above += 1;
+            max_tree_height = input[y][tree_x];
+        }
+    }
+
+    let mut count_below = 0;
+    let mut max_tree_height = 0;
+    for y in tree_y + 1..height {
+        if input[y][tree_x] > max_tree_height {
+            count_below += 1;
+            max_tree_height = input[y][tree_x];
+        }
+    }
+
+    let mut count_left = 0;
+    let mut max_tree_height = 0;
+    for x in (0..tree_x).rev() {
+        if input[tree_y][x] > max_tree_height {
+            count_left += 1;
+            max_tree_height = input[tree_y][x];
+        }
+    }
+
+    let mut count_right = 0;
+    let mut max_tree_height = 0;
+    for x in tree_x + 1..width {
+        if input[tree_y][x] > max_tree_height {
+            count_right += 1;
+            max_tree_height = input[tree_y][x];
+        }
+    }
+
+    println!(
+        "({}, {}) {} {} {} {}",
+        tree_y, tree_x, count_above, count_below, count_left, count_right
+    );
+    count_above * count_below * count_left * count_right
+    // (count_above + 1) * (count_below + 1) * (count_left + 1) * (count_right + 1)
 }
