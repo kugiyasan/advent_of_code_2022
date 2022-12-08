6aba50da21213054f613fc63da0f5fc0c6aa564b
kugiyasan <kugiyasan@users.noreply.github.com>
2022-12-08 00:43:07 -0500

    day 8 second attempt

--- d8_treetop_tree_house/src/main.rs
+++ d8_treetop_tree_house/src/main.rs
@@ -1,16 +1,8 @@
 use std::fs;
 
-fn is_inner_tree_visible(
-    input: &Vec<Vec<u32>>,
-    (x, y): (usize, usize),
-    (inner_x, inner_y): (usize, usize),
-) -> bool {
-    input[y][x] < input[inner_y][inner_x]
-}
-
 fn main() {
-    let path = "input";
-    // let path = "example";
+    // let path = "input";
+    let path = "example";
     let buf = fs::read_to_string(path).unwrap();
 
     let input: Vec<Vec<_>> = buf
@@ -19,52 +11,59 @@ fn main() {
         .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
         .collect();
 
+    // height = 99, width = 99
     let height = input.len();
     let width = input[0].len();
-    let mut res = 0;
+    // println!("height: {}, width: {}", height, width);
+    let mut seen = vec![vec![false; width]; height];
 
-    for x in 2..width - 2 {
-        if input[0][x] < input[1][x] {
-            res += 1;
+    // top and bottom
+    for x in 1..width - 1 {
+        let mut y = 0;
+        while input[y][x] < input[y + 1][x] {
+            seen[y + 1][x] = true;
+            y += 1;
         }
-        if input[height - 1][x] < input[height - 2][x] {
-            res += 1;
+
+        let mut y = height - 1;
+        while input[y][x] < input[y - 1][x] {
+            seen[y - 1][x] = true;
+            y -= 1;
         }
     }
+
+    // left and right
+    for y in 1..height - 1 {
+        let mut x = 0;
+        while input[y][x] < input[y][x + 1] {
+            seen[y][x + 1] = true;
+            x += 1;
+        }
+
+        let mut x = width - 1;
+        while input[y][x] < input[y][x - 1] {
+            println!("y: {y} x: {x}");
+            seen[y][x - 1] = true;
+            x -= 1;
+        }
+    }
+
+    println!(
+        "seen:\n{}",
+        seen.iter()
+            .map(|line| line
+                .iter()
+                .map(|&b| if b { '1' } else { '0' })
+                .collect::<String>()
+                + "\n")
+            .collect::<String>()
+    );
+    let res: usize = seen
+        .into_iter()
+        .map(|line| line.iter().filter(|&&b| b).count())
+        .sum();
     println!("{}", res);
-
-    for y in 2..height - 2 {
-        if input[0][y] < input[1][y] {
-            res += 1;
-        }
-        if input[y][width - 1] < input[y][width - 2] {
-            res += 1;
-        }
-    }
-    println!("{}", res);
-
-    // Corners
-    if input[0][1] < input[1][1] || input[1][0] < input[1][1] {
-        res += 1;
-    }
-    if is_inner_tree_visible(&input, (1, width - 1), (1, width - 2))
-        || is_inner_tree_visible(&input, (0, width - 2), (1, width - 2))
-    {
-        res += 1;
-    }
-    if is_inner_tree_visible(&input, (height - 1, 1), (height - 2, 1))
-        || is_inner_tree_visible(&input, (height - 2, 0), (height - 2, 1))
-    {
-        res += 1;
-    }
-    if input[height - 1][width - 2] < input[height - 2][width - 2]
-        || input[height - 2][width - 1] < input[height - 2][width - 2]
-    {
-        res += 1;
-    }
-    println!("{}", res);
-
     let res = res + height * 2 + (width - 2) * 2;
-    println!("{:?}", &input[0..5]);
+    println!("input:\n{}", buf);
     println!("{}", res);
 }
