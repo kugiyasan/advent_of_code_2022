af2b5c75057e4d290fd37492758381af6bb344c4
kugiyasan <kugiyasan@users.noreply.github.com>
2022-12-15 19:52:42 -0500

    day 15 part 2 got a speed bump by skipping near sensors

--- d15_beacon_exclusive_zone/src/main.rs
+++ d15_beacon_exclusive_zone/src/main.rs
@@ -2,14 +2,6 @@
 use rayon::prelude::{IndexedParallelIterator, IntoParallelIterator, ParallelIterator};
 use std::fs;
 
-#[derive(Debug, Clone)]
-enum Cell {
-    Nothing,
-    Beacon,
-    Sensor,
-    NoBeacon,
-}
-
 fn main() {
     let path = "input";
     // let path = "example";
@@ -17,18 +9,10 @@ fn main() {
     let buf = fs::read_to_string(path).unwrap();
     let input: Vec<((i32, i32), (i32, i32))> = buf.trim().split('\n').map(parse_line).collect();
 
-    let (min_x, max_x, min_y, max_y) = get_bounding_box(&input);
     let upper_bound = if path == "input" { 4000000 } else { 20 };
-
-    let min_x = min_x.max(0);
-    let max_x = max_x.min(upper_bound);
-    let min_y = min_y.max(0);
-    let max_y = max_y.min(upper_bound);
-
-    println!(
-        "min_x: {} max_x: {} min_y: {} max_y: {}",
-        min_x, max_x, min_y, max_y
-    );
+    let (min_x, max_x, min_y, max_y) = get_bounding_box(&input, upper_bound);
+    let min_y = 44000;
+    let max_y = 1000000;
 
     (min_y..=max_y)
         .collect::<Vec<_>>()
@@ -38,44 +22,45 @@ fn main() {
             println!("y: {}", range[0]);
 
             for y in range {
-                for x in min_x..=max_x {
-                    let cell = guess_cell(&input, x, y);
-                    match cell {
-                        Cell::Nothing => {
-                            let tuning_frequency = x * 4000000 + y;
-                            println!("x: {} y: {} tuning_frequency: {}", x, y, tuning_frequency);
-                        }
-                        _ => (),
-                    }
+                let mut x = min_x;
+                while x <= max_x {
+                    x += guess_cell(&input, x, y);
                 }
             }
         });
-
-    // println!("{:?}", &input[0..5]);
-    // println!("{:?}", line.iter().collect::<String>());
-    // submit("1", false);
 }
 
-fn guess_cell(input: &Vec<((i32, i32), (i32, i32))>, x: i32, y: i32) -> Cell {
+fn guess_cell(input: &Vec<((i32, i32), (i32, i32))>, x: i32, y: i32) -> i32 {
     for (sensor, beacon) in input.iter() {
         if sensor == &(x, y) {
-            return Cell::Sensor;
+            return 1;
         }
 
         if beacon == &(x, y) {
-            return Cell::Beacon;
+            return 1;
         }
 
         let dist1 = get_manhattan_distance(sensor, beacon);
         let dist2 = get_manhattan_distance(sensor, &(x, y));
         if dist1 >= dist2 {
-            return Cell::NoBeacon;
+            // println!("{:?} {:?} {} {}", (x, y), sensor, dist1, dist2);
+            let dx = sensor.0 - x;
+            if dx > 0 {
+                return dx;
+            }
+            return 1;
         }
     }
-    Cell::Nothing
+
+    let tuning_frequency = x * 4000000 + y;
+    println!("x: {} y: {} tuning_frequency: {}", x, y, tuning_frequency);
+    return 1;
 }
 
-fn get_bounding_box(input: &Vec<((i32, i32), (i32, i32))>) -> (i32, i32, i32, i32) {
+fn get_bounding_box(
+    input: &Vec<((i32, i32), (i32, i32))>,
+    upper_bound: i32,
+) -> (i32, i32, i32, i32) {
     let min_x = input
         .iter()
         .map(|((x1, _y1), (x2, _y2))| x1.min(x2))
@@ -96,6 +81,16 @@ fn get_bounding_box(input: &Vec<((i32, i32), (i32, i32))>) -> (i32, i32, i32, i3
         .map(|((_x1, y1), (_x2, y2))| y1.max(y2))
         .max()
         .unwrap();
+
+    let min_x = min_x.max(&0);
+    let max_x = max_x.min(&upper_bound);
+    let min_y = min_y.max(&0);
+    let max_y = max_y.min(&upper_bound);
+
+    println!(
+        "min_x: {} max_x: {} min_y: {} max_y: {}",
+        min_x, max_x, min_y, max_y
+    );
 
     (*min_x, *max_x, *min_y, *max_y)
 }
