57f6a03cbca83851652d3bd437b02bc4cb7d15e3
kugiyasan <kugiyasan@users.noreply.github.com>
2022-12-15 22:43:39 -0500

    cargo clippy

--- d15_beacon_exclusive_zone/src/main.rs
+++ d15_beacon_exclusive_zone/src/main.rs
@@ -1,24 +1,22 @@
 // use aoc_utils::submit;
-use rayon::{
-    prelude::{IndexedParallelIterator, IntoParallelIterator, ParallelIterator},
-    slice::ParallelSlice,
-};
 use std::fs;
+
+type Vec2 = (i32, i32);
 
 fn main() {
     let path = "input";
     // let path = "example";
 
     let buf = fs::read_to_string(path).unwrap();
-    let input: Vec<((i32, i32), (i32, i32))> = buf.trim().split('\n').map(parse_line).collect();
+    let input: Vec<(Vec2, Vec2)> = buf.trim().split('\n').map(parse_line).collect();
 
     let upper_bound = if path == "input" { 4000000 } else { 20 };
-    part2(input, upper_bound);
 
-    // visualize_grid(input);
+    visualize_grid(&input);
+    part2(input, upper_bound);
 }
 
-fn visualize_grid(input: Vec<((i32, i32), (i32, i32))>) {
+fn visualize_grid(input: &[(Vec2, Vec2)]) {
     let mut grid = vec![vec!['.'; 400]; 400];
     for (sensor, beacon) in input.iter() {
         let divider = 10000;
@@ -41,15 +39,7 @@ fn visualize_grid(input: Vec<((i32, i32), (i32, i32))>) {
     }
 }
 
-fn part2(input: Vec<((i32, i32), (i32, i32))>, upper_bound: i32) {
-    let (min_x, max_x, min_y, max_y) = get_bounding_box(&input, upper_bound);
-    // let min_y = 2000000;
-    // let max_y = 2500000;
-    // let min_y = 0;
-    // let max_y = 20;
-    // let min_y = 0;
-    // let max_y = 1000;
-
+fn part2(input: Vec<(Vec2, Vec2)>, upper_bound: i32) {
     for (sensor, beacon) in input.iter() {
         let dist = get_manhattan_distance(sensor, beacon);
         for (x, y) in get_outer_perimeter(sensor, dist) {
@@ -60,7 +50,7 @@ fn part2(input: Vec<((i32, i32), (i32, i32))>, upper_bound: i32) {
     }
 }
 
-fn get_outer_perimeter(sensor: &(i32, i32), dist: i32) -> Vec<(i32, i32)> {
+fn get_outer_perimeter(sensor: &Vec2, dist: i32) -> Vec<Vec2> {
     let mut perimeter = vec![];
 
     // far right
@@ -98,75 +88,28 @@ fn get_outer_perimeter(sensor: &(i32, i32), dist: i32) -> Vec<(i32, i32)> {
     perimeter
 }
 
-fn guess_cell(input: &Vec<((i32, i32), (i32, i32))>, x: i32, y: i32) -> i32 {
+fn guess_cell(input: &[(Vec2, Vec2)], x: i32, y: i32) {
     for (sensor, beacon) in input.iter() {
         if beacon == &(x, y) {
-            return 1;
+            return;
+        }
+
+        if sensor == &(x, y) {
+            return;
         }
 
         let dist1 = get_manhattan_distance(sensor, beacon);
-
-        if sensor == &(x, y) {
-            return dist1 + 1;
-        }
-
         let dist2 = get_manhattan_distance(sensor, &(x, y));
         if dist1 >= dist2 {
-            // println!("{:?} {:?} {} {}", (x, y), sensor, dist1, dist2);
-            let dx = sensor.0 - x;
-            // let dy = (sensor.1 - y).abs();
-            if dx > 0 {
-                return dx * 2 + 1;
-            }
-            // return dy + dx + 1;
-            return 1;
+            return;
         }
     }
 
     let tuning_frequency = x as i64 * 4000000 + y as i64;
     println!("x: {} y: {} tuning_frequency: {}", x, y, tuning_frequency);
-    return 1;
 }
 
-fn get_bounding_box(
-    input: &Vec<((i32, i32), (i32, i32))>,
-    upper_bound: i32,
-) -> (i32, i32, i32, i32) {
-    let min_x = input
-        .iter()
-        .map(|((x1, _y1), (x2, _y2))| x1.min(x2))
-        .min()
-        .unwrap();
-    let max_x = input
-        .iter()
-        .map(|((x1, _y1), (x2, _y2))| x1.max(x2))
-        .max()
-        .unwrap();
-    let min_y = input
-        .iter()
-        .map(|((_x1, y1), (_x2, y2))| y1.min(y2))
-        .min()
-        .unwrap();
-    let max_y = input
-        .iter()
-        .map(|((_x1, y1), (_x2, y2))| y1.max(y2))
-        .max()
-        .unwrap();
-
-    let min_x = min_x.max(&0);
-    let max_x = max_x.min(&upper_bound);
-    let min_y = min_y.max(&0);
-    let max_y = max_y.min(&upper_bound);
-
-    println!(
-        "min_x: {} max_x: {} min_y: {} max_y: {}",
-        min_x, max_x, min_y, max_y
-    );
-
-    (*min_x, *max_x, *min_y, *max_y)
-}
-
-fn parse_line(line: &str) -> ((i32, i32), (i32, i32)) {
+fn parse_line(line: &str) -> (Vec2, Vec2) {
     let coords = line[10..]
         .split(": closest beacon is at ")
         .map(parse_coord)
@@ -175,7 +118,7 @@ fn parse_line(line: &str) -> ((i32, i32), (i32, i32)) {
     (coords[0], coords[1])
 }
 
-fn parse_coord(coord: &str) -> (i32, i32) {
+fn parse_coord(coord: &str) -> Vec2 {
     let coord = coord
         .split(", ")
         .map(|n| n[2..].parse().unwrap())
@@ -184,7 +127,7 @@ fn parse_coord(coord: &str) -> (i32, i32) {
     (coord[0], coord[1])
 }
 
-fn get_manhattan_distance((x1, y1): &(i32, i32), (x2, y2): &(i32, i32)) -> i32 {
+fn get_manhattan_distance((x1, y1): &Vec2, (x2, y2): &Vec2) -> i32 {
     let dx = x2 - x1;
     let dy = y2 - y1;
     dx.abs() + dy.abs()
